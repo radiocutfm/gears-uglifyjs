6562ef0f63c2e24e4c86fbd7f29ef7c77ba49645
Guillermo Narvaja <guillermo.narvaja@radiocut.fm>
2020-02-21 10:55:36 -0300

    Fix javascript to make it compatible with current versions of uglifyjs.

--- gears_uglifyjs/compressor.js
+++ gears_uglifyjs/compressor.js
@@ -1,4 +1,4 @@
-var uglify = require('uglify-js'),
+var UglifyJS = require('uglify-js'),
     source = '';
 
 process.stdin.resume();
@@ -9,5 +9,5 @@ process.stdin.on('data', function(chunk) {
 });
 
 process.stdin.on('end', function() {
-  process.stdout.write(uglify(source));
+  process.stdout.write(UglifyJS.minify(source).code);
 });
